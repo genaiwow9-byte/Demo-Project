368d8eb100b7edac47404027177f220374083922
genaiwow9-byte <genaiwow9@gmail.com>
2026-08-10 17:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/new")
 def new():
-    return "FINAL CHECK"            
+    return "I am running my 1st CI pipeline"            
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
